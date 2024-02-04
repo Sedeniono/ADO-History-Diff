6a896c62ab0945337863e0c8d61dd9cec4ff02bb
Sedenion <39583823+Sedeniono@users.noreply.github.com>
2024-02-04 19:27:49 +0100

    Minify the internal css in the html.
    But only in production.

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,19 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             filename: 'historydiff.html',
-            template: './src/historydiff.html'
+            template: './src/historydiff.html',
+            minify: process.env.NODE_ENV !== 'production' ? false : {
+                // Options used by default by HtmlWebpackPlugin
+                collapseWhitespace: true,
+                keepClosingSlash: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true,
+                // Additional option compared to the default: Minimize css.
+                minifyCSS: true
+            }
         }),
     ],
 };
