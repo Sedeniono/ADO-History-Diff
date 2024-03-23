c2588a721f87da1dca61585998e26ac87de4abea
Sedenion <39583823+Sedeniono@users.noreply.github.com>
2024-03-23 12:56:57 +0100

    Added note why require() is still used for the SDK and API.

--- src/HistoryDiffPageScript.js
+++ src/HistoryDiffPageScript.js
@@ -356,6 +356,9 @@ async function InitializeHistoryDiff(adoSDK, adoAPI)
 }
 
 
+// Using 'import' instead of 'require' doesn't work with these two dependencies. The SDK gets bundled
+// by webpack twice, which causes an error because the SDK has side effects.
+// See https://stackoverflow.com/q/78210363/3740047.
 require(['azure-devops-extension-sdk', 
          'azure-devops-extension-api'
         ], 
