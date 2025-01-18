334bf81a7c2ed70760a4621fc59423dde2fa0185
Sedenion <39583823+Sedeniono@users.noreply.github.com>
2025-01-18 16:27:58 +0100

    Fixed user comments not showing up correctly in newer ADO versions.
    At least since ADO 2022: The "renderedText" property of "html" comments sometimes contains invalid image URLs; use the "text" property instead.
    At least in ADO services: The "renderedText" property is only included if the "$expand=renderedText" option is specified in the REST request.
    For the new markdown comments supported in ADO services: Show the html in the history (i.e. the "renderedText"; contains valid URLs, in contrast to "html" comments).

--- src/Comments.js
+++ src/Comments.js
@@ -6,6 +6,7 @@
 
 import { gWorkItemRESTClient } from './Globals';
 import { DiffHtmlText } from './Utils';
+import { CommentFormat } from 'azure-devops-extension-api/Comments';
 
 
 // Artificial id used for comment updates.
@@ -28,9 +29,9 @@ export function GetTableInfosForEachComment(comments)
             const curVersion = comment.allUpdates[idx];
             const prevVersion = idx !== 0 ? comment.allUpdates[idx - 1] : null;
 
-            const curText = curVersion?.isDeleted ? '' : (curVersion?.renderedText ?? curVersion?.text);
-            const prevText = prevVersion?.isDeleted ? '' : (prevVersion?.renderedText ?? prevVersion?.text);
-            const textChange = DiffHtmlText(prevText, curText);
+            const curTextHtml = GetHtmlFromComment(comment, curVersion);
+            const prevTextHtml = GetHtmlFromComment(comment, prevVersion);
+            const textChange = DiffHtmlText(prevTextHtml, curTextHtml);
 
             let action = '';
             if (idx === 0) {
@@ -65,7 +66,7 @@ export function GetTableInfosForEachComment(comments)
 export async function GetCommentsWithHistory(workItemId, projectName)
 {
     let currentPage = await GetCommentsRESTRequest(
-        workItemId, projectName, /*expand*/ 'none', undefined, /*includeDeleted*/ true);
+        workItemId, projectName, /*expand*/ 'renderedText', undefined, /*includeDeleted*/ true);
     if (!currentPage || !currentPage.comments || currentPage.comments.length == 0) {
         return [];
     }
@@ -74,7 +75,7 @@ export async function GetCommentsWithHistory(workItemId, projectName)
 
     while (currentPage.continuationToken) {
         currentPage = await GetCommentsRESTRequest(
-            workItemId, projectName, /*expand*/ 'none', undefined, 
+            workItemId, projectName, /*expand*/ 'renderedText', undefined, 
             /*includeDeleted*/ true, undefined, currentPage.continuationToken);
         if (!currentPage || !currentPage.comments || currentPage.comments.length == 0) {
             break;
@@ -107,6 +108,79 @@ export async function GetCommentsWithHistory(workItemId, projectName)
     }
 
     return allComments;
+}
+
+
+// `comment` is a `Comment`: https://learn.microsoft.com/en-us/rest/api/azure/devops/wit/comments/get-comments?view=azure-devops-rest-7.1&tabs=HTTP#comment
+// `commentOrUpdate` is either a `Comment` or a `CommentVersion` (https://learn.microsoft.com/en-us/rest/api/azure/devops/wit/comments-versions/list?view=azure-devops-rest-7.1#commentversion)
+// The `Comment` contains the `format` ("html" or "markdown"), while an `CommentVersion` does not contain the info.
+function GetHtmlFromComment(comment, commentOrUpdate)
+{
+    if (!comment || !commentOrUpdate || commentOrUpdate.isDeleted) {
+        return '';
+    }
+
+    // Currently, there are 2 possible formats in newer ADO versions: "html" and "markdown". The `renderedText` property of the 
+    // comment always contains html. The `text` property contains either html or markdown, depending on the `format`. Older ADO
+    // versions do not know the `format` property, in which case we always have html in both `text` and `renderedText`. But there
+    // are weird things going on since at least ADO 2022:
+    //
+    // - If the format is "html":
+    //   We could in principle return `renderedText`. But it turns out that at least ADO 2022 and Services return invalid URLs to
+    //   images if the user directly pasted an image into a comment. On the other hand, the `text` property does contain the full
+    //   valid HTML. For example:
+    //     `text` contains:         <img src=\"https://dev.azure.com/<organisation>/<...>/_apis/wit/attachments/e3540083-6213-4003-9f90-6fe2e982c2f4?fileName=df532c25-76d5-413d-ade5-725d633ba2f5.png\" alt=Image>
+    //     `renderedText` contains: <img src=\"ACK/e3540083-6213-4003-9f90-6fe2e982c2f4?fileName=df532c25-76d5-413d-ade5-725d633ba2f5.png\" alt=Image>
+    //   Here, ACK in `renderedText` represents a single character, ASCII code 0x06. So for some reason, ADO replaces the base URL
+    //   with ACK, but only in `renderedText`. According to https://developercommunity.visualstudio.com/t/Image-links-are-broken-for-comments-in-f/10579574,
+    //   this happens due to some "sanitization", without any explanation what that is supposed to mean or why it is done.
+    //   Also note that this ACK "sanitization" is only happening for the comments REST endpoint (GetCommentsRESTRequest()). The
+    //   versions endpoint (GetCommentsVersionsRESTRequest()) returns identical `text` and `renderedText` values with correct URLs.
+    //   => If the format is "html", we need to use the `text` property.
+    //
+    // - If the format is "markdown":
+    //   We do not support diffing markdown yet. Instead we want to return the html, which is in `renderedText`. Fortunately,
+    //   the html image URLs appear to be valid in the markdown case, for both GetCommentsRESTRequest() and 
+    //   GetCommentsVersionsRESTRequest().
+    //   Interestingly and as note for the future: `text` contains the markdown, but now with URLs "sanitized" with the ACK character,
+    //   but only for GetCommentsVersionsRESTRequest(). `text` returned by GetCommentsRESTRequest() contains the full URL.
+    //   => If the format is "markdown", we need to use the `renderedText` property.
+    //
+    // - If the comment used to be in html, and then was edited and converted to markdown by the user:
+    //   The format is now "markdown". The `renderedText` in GetCommentsRESTRequest() contains html with valid URLs, while `text` 
+    //   contains the markdown also with valid URLs.
+    //   GetCommentsVersionsRESTRequest() for those versions that are in markdown: `text` contains markdown with ACK "sanitization",
+    //   while `renderedText` contains html with correct URLs.
+    //   GetCommentsVersionsRESTRequest() for those versions that were in html: `text` and `renderedText` contain html with correct URLs.
+    //   Note: The CommentVersion (GetCommentsVersionsRESTRequest()) instances do NOT carry a `format` property, so it seems to be
+    //   impossible to know for sure when a comment was switched from html to markdown by a user.
+    //   => We use the `renderedText` property
+    //
+    // - If the comment used to be in markdown and then was edited and converted to html:
+    //   Not possible currently. ADO does not allow converting markdown comments to html.
+    // 
+    //
+    // To summarize, for future reference: 
+    //   The ACK sanitization happens for URLs in the following properties:
+    //     - format == html:     GetCommentsRESTRequest(),         `renderedText`
+    //     - format == markdown: GetCommentsVersionsRESTRequest(), `text`
+    //   Valid html with correct URLs are given by:
+    //     - format == html: GetCommentsRESTRequest(),       `text`
+    //                       GetCommentsVersionsRESTRequest, `text` and `renderedText`
+    //     - format == markdown: GetCommentsRESTRequest(),         `renderedText`
+    //                           GetCommentsVersionsRESTRequest(), `renderedText`    
+    //   Valid markdown with correct URLs are given by:
+    //     - format == html: Never
+    //     - format == markdown: GetCommentsRESTRequest(), `text`
+    //                           GetCommentsVersionsRESTRequest(): Never (`text` contains ACK)
+    // 
+    if (comment.format === undefined || comment.format === CommentFormat.Html || commentOrUpdate.renderedText === undefined) {
+        return commentOrUpdate.text; // For html, do NOT use renderedText! See above!
+    }
+    else {
+        // Probably a markdown comment, at least now (might have been html in earlier versions of the comment).
+        return commentOrUpdate.renderedText;
+    }
 }
 
 
